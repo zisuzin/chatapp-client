0b99f24fec391b1895c4b3f6b0a48e7661bc6f6f
지수진 <zi_soo_zin@naver.com>
2023-11-13 23:53:15 +0900

    app.js 업데이트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import socket from "./server";
+import { useEffect} from "react";
 
 function App() {
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+    useEffect(()=>{
+        askUserName();
+    }, []);
+    const askUserName = () => {
+        const userName = prompt("당신의 이름은?");
+        console.log("uuu", userName);
+
+        socket.emit("login", userName, (res) => {
+            console.log("Res", res);
+        });
+    };
+    return (
+        <div>
+            <div className="App"></div>
+        </div>
+    );
 }
 
 export default App;
